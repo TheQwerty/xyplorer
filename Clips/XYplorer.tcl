140912ee167adeba47e18cc574271c2fc6a988a8
TheQwerty <TheQwerty@localhost>
2008-03-19 17:34:51 +0000

    Clipbook and Syntax Definitions updated:
    Added the following variables and commands:
    	<focitem>
    	AddStr
    	Incr
    	MSecs
    	Replace
    	RegExReplace
    	Status
    	StrLen
    	StrPos
    	SubStr
    	Touch

--- Clips/XYplorer.tcl
+++ Clips/XYplorer.tcl
@@ -37,6 +37,9 @@
 !TEXT=<dyyyymmddhhnnss>
 <dyyyymmddhhnnss>
 !
+!TEXT=<focitem>
+<focitem>
+!
 !TEXT=<myyyymmddhhnnss>
 <myyyymmddhhnnss>
 !
@@ -51,6 +54,9 @@
 !
 !TEXT=#ID
 #[ID];
+!
+!TEXT=AddStr
+AddStr(VariableName, [String1=""/Blank], [String2=""/Blank], [String3=""/Blank]);
 !
 !TEXT=BackupTo
 BackupTo([Location=Current Path], [Source=Current Selection]);
@@ -83,6 +89,9 @@ GoTo(Location);
 !TEXT=Highlight
 Highlight([Color=""/Off]);
 !
+!TEXT=Incr
+Incr(VariableName, [Value="VariableName"], [Increment="1"]);
+!
 !TEXT=Input
 Input(VariableName, [Prompt=""], [DefaultValue=""], [MultiLine?:|m=""/Single]);
 !
@@ -98,6 +107,9 @@ Load(Resource, [Execute:|#[#]|[Label]=""/Display menu], [ResourceType:f|s=f/File
 !TEXT=LoadScriptFile
 //DEPRECATED: Replaced by Load();
 LoadScriptFile(ScriptFile, [Execute:|#[#]|[Label]=""/Display menu]);
+!
+!TEXT=MSecs
+MSecs([Mode:|0|1=""/Toggle]);
 !
 !TEXT=MoveTo
 MoveTo(Location, [Source=Current Selection]);
@@ -117,6 +129,12 @@ Push();
 !
 !TEXT=Rename
 Rename([Mode:bat|re|sr|kc|ext], [Pattern=""/Show Dialog], [Preview?:|p=""/Single]);
+!
+!TEXT=Repalce
+Replace(VariableName, String, Query, [Replacement], [MatchCase?:0|1]);
+!
+!TEXT=RegExReplace
+RegExReplace(VariableName, String, Pattern, [Replacement], [MatchCase?:0|1]);
 !
 !TEXT=Sel
 Sel([Type:|[|+|-][#]|"[pattern]"|a|i|f=""/Deselect All], [Count=1]);
@@ -147,6 +165,9 @@ SettingP(Setting, [Mode:1|0|r=1]);
 !TEXT=SortBy
 SortBy([Column="Name"], [Direction:|a|d|clk=Defined Default]);
 !
+!TEXT=Status
+Status(Text);
+!
 !TEXT=Step...UnStep
 Step();
 \\^
@@ -155,14 +176,26 @@ UnStep();
 !TEXT=Step
 Step();
 !
+!TEXT=StrLen
+StrLen(VariableName, String);
+!
+!TEXT=StrPos
+StrPos(VariableName, String, Query, [Start="0"], [MatchCase?:0|1]);
+!
 !TEXT=Sub
 Sub(Label);
+!
+!TEXT=SubStr
+SubStr(VariableName, String, [Start="0"], [Length="[End]"]);
 !
 !TEXT=SwapNames
 SwapNames();
 !
 !TEXT=Text
 Text(Text, [Width=500], [Height=300], [Caption=""]);
+!
+!TEXT=Touch
+Touch();
 !
 !TEXT=Try
 Try();
